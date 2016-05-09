f3b178390e1490589fada0a770dd22d72cace504
Chad Brewbaker <crb002@gmail.com>
2016-05-08 21:19:51 -0500

    Making timestamp in the record

--- meatserver/src/EventRecords.hs
+++ meatserver/src/EventRecords.hs
@@ -1,17 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
-
-{-# LANGUAGE ScopedTypeVariables         #-}
-{-# LANGUAGE OverloadedStrings           #-}
-{-# LANGUAGE ViewPatterns                #-}
-{-# LANGUAGE DeriveTraversable           #-}
-{-# LANGUAGE DeriveFoldable              #-}
-{-# LANGUAGE DeriveFunctor               #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module EventRecords where
 import Data.Time 
--- import System.Locale
-import qualified Data.Time.Format as DTF
-
+-- import qualified Data.Time.Format as DTF
 import qualified Data.ByteString.Lazy as BL
 import Data.Csv hiding (decode, decodeByName) 
 import Data.Csv.Streaming
@@ -26,16 +18,25 @@ import qualified Control.Lens as L
 data Event = Event
     { name :: !String
      ,meat :: !String
-     ,timestamp :: String
+     ,timestamp :: UTCTime
     } deriving Show
 
 
 instance FromNamedRecord Event where
     parseNamedRecord r = Event <$> r .: "person" <*> r .: "meat-bar-type"  <*> r .: "date"  
 
-getStuff :: IO [Event]
-getStuff  = do
-  csvData <-  BL.readFile "data.csv"
+instance FromField UTCTime where
+   parseField s = do
+     f <- parseField s
+     case parseTimeM True  defaultTimeLocale "%FT%T%QZ" f of
+       Nothing -> fail "Unable to parse UTC time"
+       Just g  -> return g
+
+getDefaultCSV = getStuff "data.csv"
+
+getStuff :: String -> IO [Event]
+getStuff s = do
+  csvData <-  BL.readFile s
   return $ case decodeByName  csvData of
     Left e -> error $ "data.csv " ++ show e
     Right (_,x) -> L.toListOf Tr.traverse x
